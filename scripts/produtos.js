41a08d1ac6e324369139085751e398cbcc79abf8
Bruno Furtado <bsouzacc@gmail.com>
2024-05-22 18:01:26 -0300

    primeiro commit

--- scripts/produtos.js
+++ scripts/produtos.js
@@ -14,3 +14,21 @@ const swiperProdutos = new Swiper('.produtos__carrousel', {
     },
 
 });
+
+const swiperSequencial = new Swiper('.carrousel__produtos', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    direction: 'horizontal',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
